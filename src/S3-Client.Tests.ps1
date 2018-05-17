df1465932296582d23a884a8ece5d8b473b509c0
ffeldhaus <florian.feldhaus@gmail.com>
2018-05-17 15:52:04 +0200

    Updated appveyor configuration and tests to run different configurations per AWS profile

--- src/S3-Client.Tests.ps1
+++ src/S3-Client.Tests.ps1
@@ -1,6 +1,8 @@
-Import-Module "$PSScriptRoot\S3-Client" -Force
+PARAM(
+    [Parameter(Mandatory=$false)][string]$ProfileName
+)
 
-Write-Host "Running S3 Client tests"
+Import-Module "$PSScriptRoot\S3-Client" -Force
 
 $BucketName = Get-Date -Format "yyyy-MM-dd-HHmmss"
 $UnicodeBucketName = [System.Globalization.IdnMapping]::new().GetUnicode("xn--9csy79e60h") + "-$BucketName"
@@ -8,7 +10,13 @@ $Key = "Key"
 $UnicodeKey = [System.Globalization.IdnMapping]::new().GetUnicode("xn--9csy79e60h") + "-$Key"
 $Content = "Hello World!"
 $CustomMetadata = @{"MetadataKey"="MetadataValue"}
-$Profiles = Get-AwsProfiles | Where-Object { $_.ProfileName -match "AWS|webscaledemo" }
+
+if (!$ProfileName) {
+    Write-Warning "No Profilename specified for this test, falling back to default"
+    $ProfileName = "default"
+}
+
+Write-Host "Running S3 Client tests for profile $ProfileName"
 
 # create temporary file
 $TestFileSize = 5MB
@@ -68,207 +76,203 @@ function Cleanup() {
     catch {}
 }
 
-foreach ($ProfileName in $Profiles.ProfileName) {
-    Describe "Profile $ProfileName : Get-S3Buckets" {
-        Setup
+Describe "Get-S3Buckets" {
+    Setup
 
-        Context "Retrieve buckets with default parameters" {
-            It "Retrieving buckets returns a list of all buckets" {
-                $Buckets = Get-S3Buckets -ProfileName $ProfileName
-                $Buckets.BucketName | Should -Contain $BucketName
-                $Buckets.BucketName | Should -Contain $UnicodeBucketName
-            }
+    Context "Retrieve buckets with default parameters" {
+        It "Retrieving buckets returns a list of all buckets" {
+            $Buckets = Get-S3Buckets -ProfileName $ProfileName
+            $Buckets.BucketName | Should -Contain $BucketName
+            $Buckets.BucketName | Should -Contain $UnicodeBucketName
         }
-
-        Context "Retrieve buckets with parameter -BucketName" {
-            It "Retrieving a specific, existing bucket with parameter -BucketName $BucketName returns only that bucket" {
-                $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
-                $Bucket.BucketName | Should -Be $BucketName
-            }
-
-            It "Retrieving a specific, existing bucket with parameter -BucketName $UnicodeBucketName returns only that bucket" {
-                $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $Bucket.BucketName | Should -Be $UnicodeBucketName
-            }
-        }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : Test-S3Bucket" {
-        Setup
-
-        Context "Test bucket existence with parameter -BucketName" {
-            It "Given existing bucket -BucketName $BucketName `$true is returned" {
-                Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName | Should -BeTrue
-            }
-
-            It "Given existing bucket -BucketName $UnicodeBucketName `$true is returned" {
-                Test-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName | Should -BeTrue
-            }
+    Context "Retrieve buckets with parameter -BucketName" {
+        It "Retrieving a specific, existing bucket with parameter -BucketName $BucketName returns only that bucket" {
+            $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
+            $Bucket.BucketName | Should -Be $BucketName
         }
 
-        Context "Test bucket nonexistence with parameter -BucketName" {
-            It "Given non existing bucket -BucketName non-existing-bucket `$false is returned" {
-                Test-S3Bucket -ProfileName $ProfileName -BucketName non-existing-bucket | Should -BeFalse
-            }
+        It "Retrieving a specific, existing bucket with parameter -BucketName $UnicodeBucketName returns only that bucket" {
+            $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $Bucket.BucketName | Should -Be $UnicodeBucketName
         }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : New-S3Bucket" {
-        Context "Create new bucket with default parameters" {
-            It "Given -BucketName $BucketName it is succesfully created" {
-                New-S3Bucket -ProfileName $ProfileName -BucketName $BucketName
-                foreach ($i in 1..60) {
-                    sleep 1
-                    if (Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName) {
-                        break
-                    }
+    Cleanup
+}
+
+Describe "Test-S3Bucket" {
+    Setup
+
+    Context "Test bucket existence with parameter -BucketName" {
+        It "Given existing bucket -BucketName $BucketName `$true is returned" {
+            Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName | Should -BeTrue
+        }
+
+        It "Given existing bucket -BucketName $UnicodeBucketName `$true is returned" {
+            Test-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName | Should -BeTrue
+        }
+    }
+
+    Context "Test bucket nonexistence with parameter -BucketName" {
+        It "Given non existing bucket -BucketName non-existing-bucket `$false is returned" {
+            Test-S3Bucket -ProfileName $ProfileName -BucketName non-existing-bucket | Should -BeFalse
+        }
+    }
+
+    Cleanup
+}
+
+Describe "New-S3Bucket" {
+    Context "Create new bucket with default parameters" {
+        It "Given -BucketName $BucketName it is succesfully created" {
+            New-S3Bucket -ProfileName $ProfileName -BucketName $BucketName
+            foreach ($i in 1..60) {
+                sleep 1
+                if (Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName) {
+                    break
                 }
-                $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
-                $NewBucket.BucketName | Should -Be $BucketName
             }
+            $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
+            $NewBucket.BucketName | Should -Be $BucketName
+        }
 
-            It "Given -BucketName $UnicodeBucketName it is succesfully created" {
-                New-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                foreach ($i in 1..60) {
-                    sleep 1
-                    if (Test-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName) {
-                        break
-                    }
+        It "Given -BucketName $UnicodeBucketName it is succesfully created" {
+            New-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            foreach ($i in 1..60) {
+                sleep 1
+                if (Test-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName) {
+                    break
                 }
-                $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $NewBucket.BucketName | Should -Be $UnicodeBucketName
             }
+            $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $NewBucket.BucketName | Should -Be $UnicodeBucketName
         }
+    }
 
-        Cleanup
+    Cleanup
 
-        Context "Create new bucket with parameter -UrlStyle virtual-hosted" {
-            It "Given -BucketName $BucketName and -UrlStyle virtual-hosted it is succesfully created" {
-                New-S3Bucket -ProfileName $ProfileName -BucketName $BucketName -UrlStyle virtual-hosted
-                foreach ($i in 1..60) {
-                    sleep 1
-                    if (Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName) {
-                        break
-                    }
+    Context "Create new bucket with parameter -UrlStyle virtual-hosted" {
+        It "Given -BucketName $BucketName and -UrlStyle virtual-hosted it is succesfully created" {
+            New-S3Bucket -ProfileName $ProfileName -BucketName $BucketName -UrlStyle virtual-hosted
+            foreach ($i in 1..60) {
+                sleep 1
+                if (Test-S3Bucket -ProfileName $ProfileName -BucketName $BucketName) {
+                    break
                 }
-                $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
-                $NewBucket.BucketName | Should -Be $BucketName
             }
+            $NewBucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
+            $NewBucket.BucketName | Should -Be $BucketName
         }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : Remove-S3Bucket" {
-        Setup
+    Cleanup
+}
 
-        Context "Remove bucket with default parameters" {
-            It "Given existing -BucketName $BucketName it is succesfully removed" {
-                Remove-S3Bucket -ProfileName $ProfileName -BucketName $BucketName
-                $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
-                $Bucket | Should -BeNullOrEmpty
-            }
+Describe "Remove-S3Bucket" {
+    Setup
+
+    Context "Remove bucket with default parameters" {
+        It "Given existing -BucketName $BucketName it is succesfully removed" {
+            Remove-S3Bucket -ProfileName $ProfileName -BucketName $BucketName
+            $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $BucketName
+            $Bucket | Should -BeNullOrEmpty
         }
-
-        Context "Remove bucket with default parameters" {
-            It "Given existing -BucketName $UnicodeBucketName it is succesfully removed" {
-                Remove-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $Bucket | Should -BeNullOrEmpty
-            }
-        }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : Write-S3Object" {
-        Setup
-
-        Context "Upload text" {
-            It "Given -Content `"$Content`" it is succesfully created" {
-                Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -Content $Content
-                $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName
-                $Key | Should -BeIn $Objects.Key
-                $ObjectContent = Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key
-                $ObjectContent | Should -Be $Content
-            }
+    Context "Remove bucket with default parameters" {
+        It "Given existing -BucketName $UnicodeBucketName it is succesfully removed" {
+            Remove-S3Bucket -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $Bucket = Get-S3Buckets -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $Bucket | Should -BeNullOrEmpty
         }
-
-        Context "Upload text to object with key containing unicode characters" {
-            It "Given -Content `"$Content`" it is succesfully created" {
-                Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey -Content $Content
-                $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey
-                $UnicodeKey | Should -BeIn $Objects.Key
-                $ObjectContent = Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey
-                $ObjectContent | Should -Be $Content
-            }
-        }
-
-        Context "Upload file" {
-            It "Given file -InFile `"$TestFile`" it is succesfully created" {
-                Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -InFile $TestFile
-                $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName
-                $TestFile.Name | Should -BeIn $Objects.Key
-                $TempFile = New-TemporaryFile
-                Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $TestFile.Name -OutFile $TempFile.FullName
-                $TempFileHash = $TempFile | Get-FileHash
-                $TempFileHash.Hash | Should -Be $TestFileHash.Hash
-                $TempFile | Remove-Item
-            }
-        }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : Copy-S3Object" {
-        Setup
+    Cleanup
+}
 
-        Context "Copy object" {
-            It "Given -SourceBucket $BucketName and -SourceKey $Key and -BucketName $BucketName and -Key $Key it is copied to itself" {
-                Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -Content $Content -Metadata $CustomMetadata
-                $CustomMetadata = Get-S3ObjectMetadata -ProfileName $ProfileName -BucketName $BucketName -Key $Key | Select-Object -ExpandProperty CustomMetadata
-                Copy-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -SourceBucket $BucketName -SourceKey $Key -MetadataDirective "REPLACE" -Metadata $CustomMetadata
-                Get-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key
-            }
+Describe "Write-S3Object" {
+    Setup
+
+    Context "Upload text" {
+        It "Given -Content `"$Content`" it is succesfully created" {
+            Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -Content $Content
+            $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName
+            $Key | Should -BeIn $Objects.Key
+            $ObjectContent = Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key
+            $ObjectContent | Should -Be $Content
         }
-
-        Cleanup
     }
 
-    Describe "Profile $ProfileName : S3BucketEncryption" {
-        if ($ProfileName -eq "webscaledemo") { continue }
-        Setup
-
-        Context "Set Bucket encryption" {
-            It "Given -BucketName $BucketName and -SSEAlgorithm AWS256 server side encryption is enabled" {
-                Set-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName -SSEAlgorithm AES256
-                sleep 5
-                $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
-                $BucketEncryption.SSEAlgorithm | Should -Be "AES256"
-                Remove-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
-                $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
-                $BucketEncryption | Should -BeNullOrEmpty
-            }
-
-            It "Given -BucketName $UnicodeBucketName and -SSEAlgorithm AWS256 server side encryption is enabled" {
-                Set-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName -SSEAlgorithm AES256
-                sleep 5
-                $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $BucketEncryption.SSEAlgorithm | Should -Be "AES256"
-                Remove-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
-                $BucketEncryption | Should -BeNullOrEmpty
-            }
+    Context "Upload text to object with key containing unicode characters" {
+        It "Given -Content `"$Content`" it is succesfully created" {
+            Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey -Content $Content
+            $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey
+            $UnicodeKey | Should -BeIn $Objects.Key
+            $ObjectContent = Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $UnicodeKey
+            $ObjectContent | Should -Be $Content
         }
-        
-        Cleanup
     }
 
-    
+    Context "Upload file" {
+        It "Given file -InFile `"$TestFile`" it is succesfully created" {
+            Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -InFile $TestFile
+            $Objects = Get-S3Objects -ProfileName $ProfileName -BucketName $BucketName
+            $TestFile.Name | Should -BeIn $Objects.Key
+            $TempFile = New-TemporaryFile
+            Read-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $TestFile.Name -OutFile $TempFile.FullName
+            $TempFileHash = $TempFile | Get-FileHash
+            $TempFileHash.Hash | Should -Be $TestFileHash.Hash
+            $TempFile | Remove-Item
+        }
+    }
+
+    Cleanup
+}
+
+Describe "Copy-S3Object" {
+    Setup
+
+    Context "Copy object" {
+        It "Given -SourceBucket $BucketName and -SourceKey $Key and -BucketName $BucketName and -Key $Key it is copied to itself" {
+            Write-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -Content $Content -Metadata $CustomMetadata
+            $CustomMetadata = Get-S3ObjectMetadata -ProfileName $ProfileName -BucketName $BucketName -Key $Key | Select-Object -ExpandProperty CustomMetadata
+            Copy-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key -SourceBucket $BucketName -SourceKey $Key -MetadataDirective "REPLACE" -Metadata $CustomMetadata
+            Get-S3Object -ProfileName $ProfileName -BucketName $BucketName -Key $Key
+        }
+    }
+
+    Cleanup
+}
+
+Describe "S3BucketEncryption" {
+    if ($ProfileName -eq "webscaledemo") { continue }
+    Setup
+
+    Context "Set Bucket encryption" {
+        It "Given -BucketName $BucketName and -SSEAlgorithm AWS256 server side encryption is enabled" {
+            Set-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName -SSEAlgorithm AES256
+            sleep 5
+            $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
+            $BucketEncryption.SSEAlgorithm | Should -Be "AES256"
+            Remove-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
+            $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $BucketName
+            $BucketEncryption | Should -BeNullOrEmpty
+        }
+
+        It "Given -BucketName $UnicodeBucketName and -SSEAlgorithm AWS256 server side encryption is enabled" {
+            Set-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName -SSEAlgorithm AES256
+            sleep 5
+            $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $BucketEncryption.SSEAlgorithm | Should -Be "AES256"
+            Remove-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $BucketEncryption = Get-S3BucketEncryption -ProfileName $ProfileName -BucketName $UnicodeBucketName
+            $BucketEncryption | Should -BeNullOrEmpty
+        }
+    }
+
+    Cleanup
 }
 
 $TestFile | Remove-Item
