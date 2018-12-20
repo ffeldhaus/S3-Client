237792f0a1682413b5782a75231aff71b1cbd57f
ffeldhaus <florian.feldhaus@gmail.com>
2018-12-20 16:47:53 +0100

    introduced sleep time for upload test for PowerShell 5

--- src/S3-Client.Tests.ps1
+++ src/S3-Client.Tests.ps1
@@ -350,6 +350,7 @@ Describe "Write-S3Object" {
             $TempFileHash = $TempFile | Get-FileHash
             $TempFileHash.Hash | Should -Be $SmallFileHash.Hash
             $TempFile | Remove-Item
+            sleep 1
         }
     }
 
