cb38ea8b5ba0602c3f0e371349e0336c94ac3937
Florian Feldhaus <florian.feldhaus@gmail.com>
2019-09-13 11:26:00 +0200

    Skip replication tests for webscaledemo

--- src/S3-Client.Tests.ps1
+++ src/S3-Client.Tests.ps1
@@ -901,9 +901,14 @@ Describe "S3 Bucket CORS Configuration" {
 
 Describe "S3 Bucket Replication Configuration" {
     if ($ProfileName -eq "Minio") { continue }
+    # skip these tests for webscaledemo until Platform Services are fixed by NetApp IT
+    if ($ProfileName -eq "WebscaleDemo") { continue }
 
     BeforeAll {
         if ($ProfileName -eq "Minio") { continue }
+        # skip these tests for webscaledemo until Platform Services are fixed by NetApp IT
+        if ($ProfileName -eq "WebscaleDemo") { continue }
+
         $BucketName = $BaseBucketName + "-bucket-repl"
         $UnicodeBucketName = $BaseUnicodeBucketName + "-bucket-repl"
 
@@ -931,6 +936,10 @@ Describe "S3 Bucket Replication Configuration" {
 
     AfterAll {
         if ($ProfileName -eq "Minio") { continue }
+
+        # skip these tests for webscaledemo until Platform Services are fixed by NetApp IT
+        if ($ProfileName -eq "WebscaleDemo") { continue }
+
         Cleanup -BucketName $BucketName
         Cleanup -BucketName $DestinationBucketName -Region $DestinationRegion -ProfileName AWS
 
